0fccecfdb96a06e1388f31efe3ac60f1a76c4f3a
pdeserres <pdeserres1@gmail.com>
2021-04-08 12:46:31 -0400

    On affiche pas le lien about us sur la page about

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
- <!-- si on ne donne pas le contenu titre alors par défault Hello City sera choisi en 
+    <!-- si on ne donne pas le contenu titre alors par défault Hello City sera choisi en 
     allant ds dossier config et fichier app.php et clé du tableau 'name' -->
     <title>@yield('titre', config('app.name'))</title>
 
@@ -16,7 +16,13 @@
     @yield('content')
 
     <footer>
-        <p>&copy; Copyright {{ date('y') }} &middot; <a href=" {{ route('about') }}">About Us</a></p>
+        <p>&copy; Copyright {{ date('y') }}
+            <!-- si la methode is est fausse on affiche pas le lien about 
+            si non on affiche le lien about -->
+            @if (! Route::is ('about'))
+            &middot; <a href=" {{ route('about') }}">About Us</a>
+            @endif
+        </p>
     </footer>
 </body>
 
