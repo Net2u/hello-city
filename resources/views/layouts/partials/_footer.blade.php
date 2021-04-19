e94599e023a6c109c1e81fa4eb2787a14e0c029d
pdeserres <pdeserres1@gmail.com>
2021-04-19 15:24:24 -0400

    supression du lien

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -3,12 +3,7 @@
         <div class="col">
             <div class="d-flex justify-content-center">
                 <p>&copy; Copyright {{ date('y') }}
-                    <!-- si la methode is est fausse on affiche pas le lien about 
-                    si non on affiche le lien about -->
-                    @if (! Route::is ('about'))
-                    &middot; <a href=" {{ route('about') }}">About Us</a></p>
-                @endif
-
+                </p>
             </div>
         </div>
     </div>
