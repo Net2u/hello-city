16f9bcb684ef411819af42a918e4dbe0f8252b96
pdeserres <pdeserres1@gmail.com>
2021-04-19 14:58:49 -0400

    centrer texte

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,7 +7,7 @@
             <img src=" {{asset('images/drapeau_quebec.png') }}" alt="Drapeau du Québec" class="img-fluid">
         </div>
         <div class="row justify-content-center">
-            <h1>Hello from Québec</h1>
+            <h1 class = "text-center">Hello from Québec</h1>
         </div>
         <div class="row justify-content-center">
             <p>It is currently {{ date('h:i A') }}</p>
