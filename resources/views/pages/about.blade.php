d930027a5576c6f657f4f2db3979a0f130a47590
pdeserres <pdeserres1@gmail.com>
2021-04-19 15:23:53 -0400

    suppression du lien

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -11,9 +11,6 @@
         <div class="row justify-content-center">
             <p>Built with<span style="color:rgb(234, 0, 255);font-weight:bold">&hearts;</span> by net2u</p>
         </div>
-        <div class="row justify-content-center">
-            <p><a href=" {{ route('home') }}">Page accueil</a></p>
-        </div>
     </div>
 </div>
 @endsection
