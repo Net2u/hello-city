5e1ae49db0875943147ea7b794c6a3bb8dae57e9
pdeserres <pdeserres1@gmail.com>
2021-04-26 13:43:06 -0400

    ajout flex box grid bootstrap

--- resources/views/pages/help.blade.php
+++ resources/views/pages/help.blade.php
@@ -1,13 +1,20 @@
-
 @extends('layouts.app', ['title' => 'Help'])
 
 @section('content')
-
-<h1 class = "text-center">Help</h1>
-
-<p>
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate libero dignissimos doloremque animi. Atque sit, qui animi laboriosam maxime neque laborum ab nisi a in ipsum omnis recusandae esse enim.
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, voluptate? Libero fuga quae obcaecati officia fugiat harum, asperiores voluptates? Dolorum, iste? Reprehenderit cum sit quam eius nihil nulla libero placeat.
-</p>
-
+<div class="row justify-content-center">
+    <div class="coll-10">
+        <div class="row justify-content-center">
+            <h1>Help</h1>
+        </div>
+        <div class="row justify-content-center">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate libero dignissimos doloremque animi.
+                Atque sit, qui animi laboriosam maxime neque laborum ab nisi a in ipsum omnis recusandae esse enim.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, voluptate? Libero fuga quae obcaecati
+                officia fugiat harum, asperiores voluptates? Dolorum, iste? Reprehenderit cum sit quam eius nihil nulla
+                libero placeat.
+            </p>
+        </div>
+    </div>
+</div>
 @endsection
